c45d3eb8d85653f176ed1eaa9e68018f1b4e6949
Benjamin <benjamin.lameko@gmail.com>
2023-10-27 12:28:58 +0300

    Animation

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -4,6 +4,8 @@ const modals = () => {
             modal = document.querySelector(modalSelector),
             close = document.querySelector(closeSelector),
             windows = document.querySelectorAll('[data-modal]');
+            scroll = calcScroll();
+
 
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
@@ -17,7 +19,8 @@ const modals = () => {
 
                 modal.style.display = 'block';
                 // document.body.style.overflow = 'hidden';
-                document.body.classList.add('modal-open')
+                document.body.classList.add('modal-open');
+                document.body.style.marginRight = `${scroll}px`;
             });
         });
 
@@ -28,7 +31,8 @@ const modals = () => {
 
             modal.style.display = 'none';
             // document.body.style.overflow = '';
-            document.body.classList.remove('modal-open')
+            document.body.classList.remove('modal-open');
+            document.body.style.marginRight = `0px`;
         });
 
         modal.addEventListener('click', (e) => {
@@ -39,7 +43,8 @@ const modals = () => {
                 
                 modal.style.display = 'none';
                 // document.body.style.overflow = '';
-                document.body.classList.remove('modal-open')
+                document.body.classList.remove('modal-open');
+                document.body.style.marginRight = `0px`;
             };
         });
     };
@@ -51,6 +56,20 @@ const modals = () => {
         }, time)
     };
 
+    function calcScroll() {
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWith = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWith;
+    };
     
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
